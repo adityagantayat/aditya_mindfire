b7cdcde8ae72e1e115a759800d3fbbea27958019
Aditya <adityanarayan10@gmail.com>
2021-01-25 11:09:55 +0530

    created new angular app

---
+++ Angular_Assignment/RegisterApp/src/app/RegisterForm/RegisterForm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-RegisterForm',
+  templateUrl: './RegisterForm.component.html',
+  styleUrls: ['./RegisterForm.component.scss']
+})
+export class RegisterFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
